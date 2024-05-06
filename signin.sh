4ba644d2017b64cd73bdb3482bb454e46a65595b
Divyadeep Dayal <81250915+didayal-msft@users.noreply.github.com>
2024-05-06 08:05:40 +0000

    pr check fixes

--- signin.sh
+++ signin.sh
@@ -4,3 +4,4 @@ export ARM_CLIENT_ID="ba61119e-ecef-4176-ade2-53db25293d63"
 export ARM_CLIENT_SECRET="d018Q~Qh.NGyPMRnA1ZphogiJo~JzxGspG_r6cG5"
 
 docker run --rm -v $(pwd):/src -w /src -v $HOME/.azure:/root/.azure -e TF_IN_AUTOMATION -e AVM_MOD_PATH=/src -e AVM_EXAMPLE=/examples/afd_default -e ARM_SUBSCRIPTION_ID -e ARM_TENANT_ID -e ARM_CLIENT_ID -e ARM_CLIENT_SECRET mcr.microsoft.com/azterraform:latest make afd_default
+
