4757bdfd032535a8b9d80b545f4e41b178d72ce6
Divyadeep Dayal <81250915+didayal-msft@users.noreply.github.com>
2024-05-24 05:27:29 +0000

    cdn_cd

--- variables.tf
+++ variables.tf
@@ -295,6 +295,43 @@ variable "cdn_endpoints" {
   nullable    = false
 }
 
+variable "cdn_endpoint_custom_domains" {
+  type = map(object({
+    cdn_endpoint_key = string
+    host_name        = string
+    name             = string
+    cdn_managed_https = optional(object({
+      certificate_type = string
+      protocol_type    = string
+      tls_version      = optional(string, "TLS12")
+    }))
+    user_managed_https = optional(object({
+      key_vault_certificate_id = optional(string)
+      key_vault_secret_id      = optional(string)
+      tls_version              = optional(string)
+    }))
+  }))
+  description = <<-EOT
+ - `cdn_endpoint_key` - (Required) key of the endpoint defined in variable cdn_endpoints.
+ - `host_name` - (Required) The host name of the custom domain. Changing this forces a new CDN Endpoint Custom Domain to be created.
+ - `name` - (Required) The name which should be used for this CDN Endpoint Custom Domain. Changing this forces a new CDN Endpoint Custom Domain to be created.
+
+ ---
+ `cdn_managed_https` block supports the following:
+ - `certificate_type` - (Required) The type of HTTPS certificate. Possible values are `Shared` and `Dedicated`.
+ - `protocol_type` - (Required) The type of protocol. Possible values are `ServerNameIndication` and `IPBased`.
+ - `tls_version` - (Optional) The minimum TLS protocol version that is used for HTTPS. Possible values are `TLS10` (representing TLS 1.0/1.1), `TLS12` (representing TLS 1.2) and `None` (representing no minimums). Defaults to `TLS12`.
+
+ `user_managed_https` block supports the following:
+ - `key_vault_certificate_id` - (Optional) The ID of the Key Vault Certificate that contains the HTTPS certificate. This is deprecated in favor of `key_vault_secret_id`.
+ - `key_vault_secret_id` - (Optional) The ID of the Key Vault Secret that contains the HTTPS certificate.
+ - `tls_version` - (Optional) The minimum TLS protocol version that is used for HTTPS. Possible values are `TLS10` (representing TLS 1.0/1.1), `TLS12` (representing TLS 1.2) and `None` (representing no minimums). Defaults to `TLS12`.
+EOT
+  nullable    = false
+  default     = {}
+}
+
+
 variable "diagnostic_settings" {
   type = map(object({
     name                                     = optional(string, null)
@@ -453,6 +490,8 @@ variable "front_door_endpoints" {
   DESCRIPTION
   nullable    = false
 }
+
+
 
 variable "front_door_firewall_policies" {
   type = map(object({
@@ -1296,6 +1335,7 @@ variable "front_door_rules" {
   default     = {}
   description = <<DESCRIPTION
   Manages a Front Door (standard/premium) Rules. Please review documentation here for usage. https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/cdn_frontdoor_rule
+  
   Example Input:
 
   ```terraform
